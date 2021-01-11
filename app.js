01637a37fd53084eb3234dc63203d3834cead9ed
TiesHogenboom <70697022+TiesHogenboom@users.noreply.github.com>
2021-01-11 13:08:04 +0100

    plaatjes bijgevoegd

--- app.js
+++ app.js
@@ -2,11 +2,7 @@
 const controls = new Controls(50);
 controls.enableMovement();
 
-// create a model
-const melkMeisje = new Model('assets/models/vermeer/export_museumprops.babylon.gltf');
-melkMeisje.setPosition(-0.293, -0.05, -2.57);
-melkMeisje.setScale(100, 100, 100);
-melkMeisje.setRotation(0, 45.45, 0);
+
 
 // create a model
 const cuberoom = new Model('assets/models/cube-room/cube-room.gltf');
@@ -17,41 +13,60 @@ cuberoom.setRotation(-0.76, -36.95, 0);
 
 // create a primitive (sphere)
 const sphere = new Sphere();
-sphere.setColor('green');
-sphere.setRadius(0.5);
-sphere.setPosition(-3, 0, -5);
+sphere.setColor('white');
+sphere.setRadius(0.3);
+sphere.setPosition(0, 1, -2);
 
 // create some text
 const lamText = new Text('Lisser Art Museum');
-lamText.setPosition(-0.1, 2.2, -2.9);
-lamText.setFontsize(10);
+lamText.setPosition(-0.9, 2.2, -2.1);
+lamText.setFontsize(12);
 lamText.setScale(0.5, 0.5, 0.5)
-lamText.setRotation(0, -38, 0);
+lamText.setRotation(0, -13, 0);
+
+// create some text
+const lamText1 = new Text('Click me');
+lamText1.setPosition(-0.5, 1.6, -2.1);
+lamText1.setFontsize(12);
+lamText1.setScale(0.5, 0.5, 0.5)
+lamText1.setRotation(0, -11, 0);
 
 // create an image
-const image1 = new XRImage('assets/images/Lisse-049.jpg');
-image1.setPosition(-1.4, 1.9, -1.4);
-image1.setRotation(-0.74, 43, 178);
-image1.setScale(1.5, 1.5, 1.5);
+const image1 = new XRImage('assets/images/FolkertdeJong1.jpg');
+image1.setPosition(-1.55, 1.9, -1.4);
+image1.setRotation(0, 53, 0);
+image1.setScale(2, 2, 2);
 
 // create an imageí
-const image2 = new XRImage('assets/images/FolkertdeJong.jpg');
-image2.setPosition(2, 1.9, -1);
-image2.setRotation(0, -39, -1);
-image2.setScale(1.5, 1.5, 1.5);
+const image2 = new XRImage('assets/images/FolkertdeJong2.jpg');
+image2.setPosition(2, 1.9, -1.05);
+image2.setRotation(0, -37, 0);
+image2.setScale(1.8, 1.8, 1.8);
 
 // create an imageí
-const image3 = new XRImage('assets/images/Lisse-043.jpg');
+const image3 = new XRImage('assets/images/FolkertdeJong3.jpg');
 image3.setPosition(2.1, 1.9, 0.7);
 image3.setRotation(0, -127, 0);
 image3.setScale(1.5, 1.5, 1.5);
+
+// create an imageí
+const image4 = new XRImage('assets/images/FolkertdeJong4.jpg');
+image4.setPosition(1.1, 1.9, 2.1);
+image4.setRotation(0, -127, 0);
+image4.setScale(1.5, 1.5, 1.5);
+
+// create an imageí
+const image5 = new XRImage('assets/images/FolkertdeJong5.jpg');
+image5.setPosition(-1.3, 1.9, 1.3);
+image5.setRotation(0, -220, 0);
+image5.setScale(2, 2, 2);
 
 
 // create a 360 image
 const image360 = new Sky('assets/LamIngang.jpg');
 
 const spotLight = new SpotLight();
-spotLight.setPosition(-1, 2, 3.7);
+spotLight.setPosition(-0.2, 2, 3.7);
 spotLight.setTarget(melkMeisje);
 
 // listen for an event
